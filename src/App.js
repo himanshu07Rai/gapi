c8934aa11d1e3cf7466b3353ca17b1519a3375bb
Himanshu Rai <raihimanshu22345gmail.com>
2021-01-27 13:19:29 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         .signIn()
         .then(() => {
           var event = {
-            summary: "Google I/O 2015",
+            summary: "Google I/O",
             location: "800 Howard St., San Francisco, CA 94103",
             description:
               "A chance to hear more about Google's developer products.",
@@ -38,22 +38,10 @@ function App() {
               dateTime: "2021-01-28T09:00:00-07:00",
               timeZone: "America/Los_Angeles",
             },
-            end: {
-              dateTime: "2021-01-28T17:00:00-07:00",
-              timeZone: "America/Los_Angeles",
-            },
-            recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
             attendees: [
               { email: "lpage@example.com" },
               { email: "sbrin@example.com" },
             ],
-            reminders: {
-              useDefault: false,
-              overrides: [
-                { method: "email", minutes: 24 * 60 },
-                { method: "popup", minutes: 10 },
-              ],
-            },
           };
 
           var request = gapi.client.calendar.events.insert({
